8549ac2aec08f871e3573456056db0014976b7fc
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-02-18 17:44:02 +0400

    Product Detail and Product Image Service is added (20)

--- Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -1,6 +1,14 @@
-﻿namespace MultiShop.Catalog.Services.CategoryServices
+﻿using MultiShop.Catalog.Dtos.CategoryDtos;
+
+namespace MultiShop.Catalog.Services.CategoryServices
 {
     public interface ICategoryService
     {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto>GetByIdCategoryAsync(string id);
+
     }
 }
