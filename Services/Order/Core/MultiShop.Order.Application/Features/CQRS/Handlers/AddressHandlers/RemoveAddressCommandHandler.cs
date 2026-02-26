22936b5421f08f356d078a567353a08f844564f0
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-02-26 17:54:24 +0400

    Onion Architecture & CQRS Mediator and  Order Microservices is added to project

--- Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/RemoveAddressCommandHandler.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/RemoveAddressCommandHandler.cs
@@ -1,12 +1,6 @@
 ﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
 using MultiShop.Order.Application.Interfaces;
 using MultiShop.Order.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
 {
     public class RemoveAddressCommandHandler
--- Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Linq.Expressions;
 namespace MultiShop.Order.Application.Interfaces
 {
     public interface IRepository<T>  where T : class
     {
         Task<List<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
-        Task<T> CreateAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(T entity);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
         Task<T>GetByFilterAsync(Expression<Func<T, bool>> filter);
     }
 }
