d4c1db320dfe843455fc3b62d4bb1e5a4abc45a9
n.a.ismayilzada <n.a.ismayilzada@RB00-IT028>
2026-02-27 16:05:48 +0400

    Mediatr is added

---
+++ Services/Order/Core/MultiShop.Order.Application/Services/ServiceRegistiration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MultiShop.Order.Application.Services
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(ServiceRegistiration).Assembly));
+        }
+    }
+}
